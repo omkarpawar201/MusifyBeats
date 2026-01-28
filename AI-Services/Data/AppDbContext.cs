98f5c88ce2413e48167d088daa1871381e51fed1
Pratik Chikane <pratikchikane27@gmail.com>
2026-01-28 23:27:48 +0530

    Initial commit of AI-Services

---
+++ AI-Services/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using AI_Services.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AI_Services.Data
+{
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Song> Songs { get; set; } = null!;
+        }
+    
+
+}
